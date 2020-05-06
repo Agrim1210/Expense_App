c073bd5389b96ada84e732f33aa7b6b66e5a7096
Agrim Gupta <agrim121@gmail.com>
2020-05-06 22:29:23 +0530

    Transaction added; git status clear
    git revert -h

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:expense_app/transaction.dart';
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,25 +15,43 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Column(children: <Widget>[
-        Card(
-          child: Container(
-            width: 1000,
-            color: Colors.limeAccent,
-            child: Text('CHART!'),
-          ),
-          elevation: 5,
-        ),
-        Card(
-          child: Text('LIST OF TX'),
-        ),
-      ]),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 1000,
+              child: Card(
+                color: Colors.limeAccent,
+                child: Text('CHART!'),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              color: Colors.red,
+              child: Text('LIST OF TX'),
+            ),
+          ]),
     );
   }
 }
