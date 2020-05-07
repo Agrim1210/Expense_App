2121b6366a0cee0c30e6df355d2fa55249ee82e5
Agrim Gupta <agrim121@gmail.com>
2020-05-08 01:02:57 +0530

    Adding Option modified

--- lib/widgets/user_transaction.dart
+++ lib/widgets/user_transaction.dart
@@ -1,46 +1,24 @@
-import 'package:flutter/material.dart';
-import './new_transaction.dart';
-import './transation_list.dart';
-import '../models/transaction.dart';
+// import 'package:flutter/material.dart';
+// import './new_transaction.dart';
+// import './transation_list.dart';
+// import '../models/transaction.dart';
 
-class UserTransaction extends StatefulWidget {
-  @override
-  _UserTransactionState createState() => _UserTransactionState();
-}
+// class UserTransaction extends StatefulWidget {
+//   @override
+//   _UserTransactionState createState() => _UserTransactionState();
+// }
 
-class _UserTransactionState extends State<UserTransaction> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't1',
-      title: 'Weekly Groceries',
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
-  ];
-  void _addNewTransaction(String txTitle, double txAmount) {
-    final newTx = Transaction(
-        id: DateTime.now().toString(),
-        amount: txAmount,
-        date: DateTime.now(),
-        title: txTitle);
-    setState(() {
-      _userTransactions.add(newTx);
-    });
-  }
+// class _UserTransactionState extends State<UserTransaction> {
+ 
+  
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        NewTransaction(_addNewTransaction),
-        TransactionList(_userTransactions),
-      ],
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: <Widget>[
+//         NewTransaction(_addNewTransaction),
+      
+//       ],
+//     );
+//   }
+// }
