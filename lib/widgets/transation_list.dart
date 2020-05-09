1e5618d448f186474c84093e56c055a50716a33a
Agrim Gupta <agrim121@gmail.com>
2020-05-09 12:45:00 +0530

    Image added in case of no transaction

--- lib/widgets/transation_list.dart
+++ lib/widgets/transation_list.dart
@@ -10,57 +10,74 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 400,
-        child:ListView.builder(
-          itemBuilder: (ctx,index){
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Theme.of(context).primaryColor,
-                          width: 2,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        //  '₹ ${tx.amount}',
-                        '\$ ${transactions[index].amount}',
-
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ),
-                    ),
-                    Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                           transactions[index].title,
-                            style:Theme.of(context).textTheme.title,
-                          ),
-                          Text(
-                            DateFormat('dd/MM/yyyy').format(transactions[index].date),
-                            style: TextStyle(
-                              color: Colors.brown,
-                            ),
-                          ),
-                        ])
-                  ],
+      height: 400,
+      child: transactions.isEmpty
+          ? Column(
+              children: <Widget>[
+                Text(
+                  'No transactions added yet!',
+                  style: Theme.of(context).textTheme.title,
                 ),
-              );
-          },
-          itemCount: transactions.length,
-            
-          ),
-        
-        );
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ))
+              ],
+            )
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 15,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Theme.of(context).primaryColor,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          //  '₹ ${tx.amount}',
+                          '\$ ${transactions[index].amount}',
+
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        ),
+                      ),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              transactions[index].title,
+                              style: Theme.of(context).textTheme.title,
+                            ),
+                            Text(
+                              DateFormat('dd/MM/yyyy')
+                                  .format(transactions[index].date),
+                              style: TextStyle(
+                                color: Colors.brown,
+                              ),
+                            ),
+                          ])
+                    ],
+                  ),
+                );
+              },
+              itemCount: transactions.length,
+            ),
+    );
   }
 }
