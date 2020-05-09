93e5185d5386b3e2ea32ba04313fb6fc4ea0a269
Agrim Gupta <agrim121@gmail.com>
2020-05-10 00:44:30 +0530

    Adding date with custom date is enabled

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -12,19 +12,22 @@ class NewTransaction extends StatefulWidget {
 
 class _NewTransactionState extends State<NewTransaction> {
   final _titleController = TextEditingController();
-
   final _amountController = TextEditingController();
   DateTime _selectedDate;
 
   void _submitData() {
+    if(_amountController.text.isEmpty){
+      return;
+    }
     final enteredTitle = _titleController.text;
     final enteredAmount = double.parse(_amountController.text);
-    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0 ||_selectedDate==null              ) {
       return;
     }
     widget.addTx(
       enteredTitle,
       enteredAmount,
+      _selectedDate,
     );
     Navigator.of(context).pop();
   }
