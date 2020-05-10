d93859dec91158ef78a13d46be135e142eb3a8f6
Agrim Gupta <agrim121@gmail.com>
2020-05-10 23:29:32 +0530

    Size adjusted and made dynamic

--- lib/widgets/transation_list.dart
+++ lib/widgets/transation_list.dart
@@ -10,9 +10,7 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      child: transactions.isEmpty
+    return transactions.isEmpty
           ? Column(
               children: <Widget>[
                 Text(
@@ -68,7 +66,7 @@ class TransactionList extends StatelessWidget {
                 );
               },
               itemCount: transactions.length,
-            ),
+            
     );
   }
 }
